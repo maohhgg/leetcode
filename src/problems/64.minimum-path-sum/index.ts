0ae897e413c3643089102b644acd69abb0ad9af1
maohhgg <maohhgg@gmail.com>
2019-10-18 15:30:53 +0800

    64. Minimum Path Sum

---
+++ src/problems/64.minimum-path-sum/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const minPathSum = (grid: number[][]): number => {
+    for (let i = grid.length - 1; i >= 0; i--) {
+        for (let j = grid[0].length - 1; j >= 0; j--) {
+            if(i == grid.length - 1 && j != grid[0].length - 1)
+                grid[i][j] = grid[i][j] +  grid[i][j + 1];
+            else if(j == grid[0].length - 1 && i != grid.length - 1)
+                grid[i][j] = grid[i][j] + grid[i + 1][j];
+            else if(j != grid[0].length - 1 && i != grid.length - 1)
+                grid[i][j] = grid[i][j] + Math.min(grid[i + 1][j],grid[i][j + 1]);
+        }
+    }
+    return grid[0][0];
+};
+
+let t1:number[][] = [[1,3,1],[1,5,1], [4,2,1]];
+console.log(minPathSum(t1));
